fdd925d0892a54b4f2695b5ba04f3d9dacfd8e84
HemantDutta <hemantnorddutta2@gmail.com>
2022-12-18 18:58:32 +0530

    v0.1.a

--- scripts.js
+++ scripts.js
@@ -16,21 +16,30 @@ function suggest(val) {
 }
 
 function generateMeme(subreddit) {
-    fetch(`https://meme-api.com/gimme/${subreddit}`).then(res => res.json()).then(data => {
-        console.log(data);
-        memeData = data;
-        document.getElementById('memeCont').classList.remove('d-none');
-        memeImg.innerHTML = `<img src="${memeData.url}" alt="meme-image" width="100%">`;
-        memeTitle.innerText = memeData.title;
-        memeAuthor.innerText = memeData.author;
-        memeLink.innerHTML = `<a href="${memeData.postLink}" target="_blank" class="memeLink">Vist this post on reddit <i class="fa-brands fa-reddit-alien"></i></a>`;
-        if (memeData.nsfw) {
-            document.getElementById('nsfwWarn').innerText = "NSFW WARNING!"
-        } else {
-            document.getElementById('nsfwWarn').innerText = "SFW"
-        }
-        if (memeData.spoiler) {
-            document.getElementById('spoilerWarn').innerText = "SPOILER WARNING!"
-        }
-    });
+    let subs = ['memes', 'dankmemes', 'funny', 'jokes', 'comedy', 'notfunny'];
+
+    if(subs.includes(subreddit)){
+        fetch(`https://meme-api.com/gimme/${subreddit}`).then(res => res.json()).then(data => {
+            console.log(data);
+            memeData = data;
+            document.getElementById('memeCont').classList.remove('d-none');
+            memeImg.innerHTML = `<img src="${memeData.url}" alt="meme-image" width="100%">`;
+            memeTitle.innerText = memeData.title;
+            memeAuthor.innerText = memeData.author;
+            memeLink.innerHTML = `<a href="${memeData.postLink}" target="_blank" class="memeLink">Vist this post on reddit <i class="fa-brands fa-reddit-alien"></i></a>`;
+            if (memeData.nsfw) {
+                document.getElementById('nsfwWarn').innerText = "NSFW WARNING!"
+            } else {
+                document.getElementById('nsfwWarn').innerText = "SFW"
+            }
+            if (memeData.spoiler) {
+                document.getElementById('spoilerWarn').innerText = "SPOILER WARNING!"
+            }
+        });
+    }
+    else{
+        alert('Please only type in comedy related subreddits');
+    }
+
+
 }
