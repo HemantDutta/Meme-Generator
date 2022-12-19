2297be3b3bfeb4ff4914af258381f764b761f536
HemantDutta <hemantnorddutta2@gmail.com>
2022-12-19 21:54:37 +0530

    safe mode initialize

--- scripts.js
+++ scripts.js
@@ -1,13 +1,21 @@
 let memeData;
 let subreddit;
+let safe;
 let memeImg = document.getElementById('meme-img');
 let memeTitle = document.getElementById('memeTitle');
 let memeAuthor = document.getElementById('memeAuthor');
 let memeLink = document.getElementById('redditLink');
 
+
 function search() {
     subreddit = document.getElementById('subreddit').value;
-    generateMeme(subreddit);
+    if(safe){
+        generateMeme(subreddit);
+    }
+    else{
+        generatePost(subreddit);
+    }
+
 }
 
 function suggest(val) {
@@ -15,10 +23,14 @@ function suggest(val) {
     generateMeme(val);
 }
 
-function generateMeme(subreddit) {
-    let subs = ['memes', 'dankmemes', 'funny', 'jokes', 'comedy', 'notfunny'];
+function safeMode(){
+    
+}
 
-    if(subs.includes(subreddit)){
+function generateMeme(subreddit) {
+    let subs = ['memes', 'dankmemes', 'funny', 'jokes', 'comedy', 'notfunny', 'comedyheaven', 'bonehurtingjuice', 'comedycemetery', 'comedynecrophilia', 'okbuddyretards', 'deepfriedmemes', 'nukedmemes', 'blackholedmemes', 'meme', 'memes_of_the_dank'];
+
+    if(subs.includes(subreddit.toLowerCase())){
         fetch(`https://meme-api.com/gimme/${subreddit}`).then(res => res.json()).then(data => {
             console.log(data);
             memeData = data;
@@ -40,6 +52,24 @@ function generateMeme(subreddit) {
     else{
         alert('Please only type in comedy related subreddits');
     }
+}
 
-
+function generatePost(subreddit) {
+    fetch(`https://meme-api.com/gimme/${subreddit}`).then(res => res.json()).then(data => {
+        console.log(data);
+        memeData = data;
+        document.getElementById('memeCont').classList.remove('d-none');
+        memeImg.innerHTML = `<img src="${memeData.url}" alt="meme-image" width="100%">`;
+        memeTitle.innerText = memeData.title;
+        memeAuthor.innerText = memeData.author;
+        memeLink.innerHTML = `<a href="${memeData.postLink}" target="_blank" class="memeLink">Vist this post on reddit <i class="fa-brands fa-reddit-alien"></i></a>`;
+        if (memeData.nsfw) {
+            document.getElementById('nsfwWarn').innerText = "NSFW WARNING!"
+        } else {
+            document.getElementById('nsfwWarn').innerText = "SFW"
+        }
+        if (memeData.spoiler) {
+            document.getElementById('spoilerWarn').innerText = "SPOILER WARNING!"
+        }
+    });
 }
